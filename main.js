022a045543b03eb0679c3cba134663342dfb373e
Joni Laukka <jonilaukka@hotmail.com>
2019-10-20 20:18:26 +0300

    Infobox added under the map panel

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.on("ready", () => {
   // Notes
   createWindow({ width: 1200, height: 750 }, "index.html");
   // Map
-  createWindow({ width: 1800, height: 900 }, "map.html");
+  createWindow({ width: 1800, height: 880 }, "map.html");
 });
 
 app.on("window-all-closed", () => {
